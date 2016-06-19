b807f7bf7af759e8caf7cce2dff3f3a8f07e8604
Sean Smith <sean.smith.2009@gmail.com>
2016-06-19 12:30:02 -0400

    updated index.js files to ES6

--- front-end-projects/1_quote/js/index-compiled.js
+++ front-end-projects/1_quote/js/index-compiled.js
@@ -1,19 +1,45 @@
-'use strict';
+"use strict";
 
-var Root = React.createClass({
-  displayName: 'Root',
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-  render: function render() {
-    return React.createElement(
-      'div',
-      null,
-      React.createElement(
-        'h1',
-        null,
-        'Welcome to React'
-      )
-    );
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Root = function (_React$Component) {
+  _inherits(Root, _React$Component);
+
+  function Root() {
+    _classCallCheck(this, Root);
+
+    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(Root).call(this));
+
+    _this.state = {
+      name: "Sean"
+    };
+    return _this;
   }
-});
+
+  _createClass(Root, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "h1",
+          null,
+          "Welcome to React in ES6 World"
+        )
+      );
+    }
+  }]);
+
+  return Root;
+}(React.Component);
+
+;
 
 ReactDOM.render(React.createElement(Root, null), document.getElementById('main'));
--- front-end-projects/1_quote/js/index.js
+++ front-end-projects/1_quote/js/index.js
@@ -1,12 +1,18 @@
 
-var Root = React.createClass({
-  render: function() {
+class Root extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Sean"
+    };
+  }
+  render() {
     return (
       <div>
-        <h1>Welcome to React</h1>
+        <h1>Welcome to React in ES6 World</h1>
       </div>
       );
   }
-});
+};
 
 ReactDOM.render(<Root />, document.getElementById('main'));
