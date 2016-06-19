521e0178dd1c312147054b0470e94aef7224baf1
Sean Smith <sean.smith.2009@gmail.com>
2016-06-19 01:48:34 -0400

    added project files

---
+++ project-template/js/index.js
@@ -0,0 +1,12 @@
+
+var Root = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Welcome to this terrible World</h1>
+      </div>
+      );
+  }
+});
+
+ReactDOM.render(<Root />, document.getElementById('main'));
