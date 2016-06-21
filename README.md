d516f36b8787f08f40892fc8d0a638dcf158cf87
Sean Smith <sean.smith.2009@gmail.com>
2016-06-21 00:28:03 -0400

    added calculator project

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ This repository contains the Intermediate and Advanced Projects from Free Code C
 
 **I used Toggl to record the time spent on each of the projects. The results are:**
 
-1. Random Quote Generator  => 1:33:27
-2. Weather API Service     => 2:24:28
-3. Wikipedia Viewer        => 3:22:58
-4. Twitch API Service      => 4:18:52
-5. Javascript Calculator   =>
-6. Pomodoro Clock          =>
-7. Tic-Tac-Toe Game        =>
-8. Simon Game              =>
+1. Random Quote Generator&nbsp;&nbsp;=> 1:33:27
+2. Weather API Service&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=> 2:24:28
+3. Wikipedia Viewer&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=> 3:22:58
+4. Twitch API Service&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=> 4:18:52
+5. Javascript Calculator&nbsp;&nbsp;&nbsp;=> 1:4618
+6. Pomodoro Clock&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=>
+7. Tic-Tac-Toe Game&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=>
+8. Simon Game&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=>
---
+++ front-end-projects/5_calculator/js/index-compiled.js
@@ -0,0 +1,263 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Root = function (_React$Component) {
+  _inherits(Root, _React$Component);
+
+  function Root() {
+    _classCallCheck(this, Root);
+
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(Root).apply(this, arguments));
+  }
+
+  _createClass(Root, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(CalcComponent, null)
+      );
+    }
+  }]);
+
+  return Root;
+}(React.Component);
+
+;
+
+var CalcComponent = function (_React$Component2) {
+  _inherits(CalcComponent, _React$Component2);
+
+  function CalcComponent() {
+    _classCallCheck(this, CalcComponent);
+
+    var _this2 = _possibleConstructorReturn(this, Object.getPrototypeOf(CalcComponent).call(this));
+
+    _this2.state = {
+      data: ''
+    };
+    return _this2;
+  }
+
+  _createClass(CalcComponent, [{
+    key: 'handleChange',
+    value: function handleChange(event) {
+      var currentLength = this.state.data.toString().length;
+      var current;
+      var update;
+      var input = event;
+
+      // Check if calculator is in intial state:
+      if (currentLength === 0) {
+        current = '';
+        update = input;
+      }
+
+      // Check if current value is less than length length:
+      else if (currentLength > 0 && currentLength < 20) {
+          current = this.state.data.toString().slice(0);
+          update = current.toString() + input.toString();
+        }
+
+        // If input is at length limit, do not allow any additions:
+        else if (currentLength === 20) {
+            input = '';
+            update = this.state.data.toString().slice(0);
+          }
+
+      this.setState({
+        data: update
+      });
+    }
+  }, {
+    key: 'delete',
+    value: function _delete() {
+      var deleted = this.state.data.toString().slice(0, this.state.data.toString().length - 1);
+      this.setState({
+        data: deleted
+      });
+    }
+  }, {
+    key: 'clearInput',
+    value: function clearInput() {
+      this.setState({
+        data: ''
+      });
+    }
+  }, {
+    key: 'evaluate',
+    value: function evaluate() {
+      var expression = this.state.data.slice(0);
+      var answer = '';
+
+      try {
+        eval(expression);
+      } catch (error) {
+        if (error instanceof SyntaxError) {
+          answer = "Bad syntax!";
+        }
+      } finally {
+        if (answer.length === 0) {
+          answer = eval(expression);
+        }
+      }
+
+      this.setState({
+        data: answer
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'calcWrapper' },
+        React.createElement(
+          'div',
+          { className: 'case' },
+          React.createElement(
+            'p',
+            { className: 'title' },
+            'Javascript Calculator'
+          ),
+          React.createElement('input', {
+            className: 'inputField',
+            type: 'text',
+            value: this.state.data }),
+          React.createElement(CalcDisplay, {
+            input: this.handleChange.bind(this),
+            'delete': this.delete.bind(this),
+            clear: this.clearInput.bind(this),
+            enter: this.evaluate.bind(this) })
+        )
+      );
+    }
+  }]);
+
+  return CalcComponent;
+}(React.Component);
+
+;
+
+var CalcDisplay = function (_React$Component3) {
+  _inherits(CalcDisplay, _React$Component3);
+
+  function CalcDisplay() {
+    _classCallCheck(this, CalcDisplay);
+
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(CalcDisplay).apply(this, arguments));
+  }
+
+  _createClass(CalcDisplay, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'div',
+          { className: 'calcUI' },
+          React.createElement(
+            'button',
+            { className: 'numberBtn', onClick: this.props.input.bind(this, "7") },
+            '7'
+          ),
+          React.createElement(
+            'button',
+            { className: 'numberBtn', onClick: this.props.input.bind(null, "8") },
+            '8'
+          ),
+          React.createElement(
+            'button',
+            { className: 'numberBtn', onClick: this.props.input.bind(null, "9") },
+            '9'
+          ),
+          React.createElement(
+            'button',
+            { className: 'operationBtn', onClick: this.props.input.bind(null, "/") },
+            '/'
+          ),
+          React.createElement(
+            'button',
+            { className: 'numberBtn', onClick: this.props.input.bind(null, "4") },
+            '4'
+          ),
+          React.createElement(
+            'button',
+            { className: 'numberBtn', onClick: this.props.input.bind(null, "5") },
+            '5'
+          ),
+          React.createElement(
+            'button',
+            { className: 'numberBtn', onClick: this.props.input.bind(null, "6") },
+            '6'
+          ),
+          React.createElement(
+            'button',
+            { className: 'operationBtn', onClick: this.props.input.bind(null, "*") },
+            '*'
+          ),
+          React.createElement(
+            'button',
+            { className: 'numberBtn', onClick: this.props.input.bind(null, "1") },
+            '1'
+          ),
+          React.createElement(
+            'button',
+            { className: 'numberBtn', onClick: this.props.input.bind(null, "2") },
+            '2'
+          ),
+          React.createElement(
+            'button',
+            { className: 'numberBtn', onClick: this.props.input.bind(null, "3") },
+            '3'
+          ),
+          React.createElement(
+            'button',
+            { className: 'operationBtn', onClick: this.props.input.bind(null, "+") },
+            '+'
+          ),
+          React.createElement(
+            'button',
+            { className: 'delBtn', onClick: this.props.delete.bind(this) },
+            'del'
+          ),
+          React.createElement(
+            'button',
+            { className: 'numberBtn', onClick: this.props.input.bind(null, "0") },
+            '0'
+          ),
+          React.createElement(
+            'button',
+            { className: 'clearBtn', onClick: this.props.clear.bind(this) },
+            'CE'
+          ),
+          React.createElement(
+            'button',
+            { className: 'operationBtn', onClick: this.props.input.bind(null, "-") },
+            '-'
+          )
+        ),
+        React.createElement(
+          'button',
+          { onClick: this.props.enter.bind(this), className: 'enterBtn' },
+          'ENTER'
+        )
+      );
+    }
+  }]);
+
+  return CalcDisplay;
+}(React.Component);
+
+;
+
+ReactDOM.render(React.createElement(Root, null), document.getElementById('main'));
